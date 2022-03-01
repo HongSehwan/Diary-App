3a1c47d0d52e0612dc373eb4cf550490e15e1100
Hong <gg9297@gmail.com>
2022-03-01 18:20:33 +0900

    Add data

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import React from "react";
+import Realm from "realm";
+import React, { useState } from "react";
+import AppLoading from "expo-app-loading";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigator from "./navigator";
 
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id",
+};
+
+const Context = React.createContext();
+
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const [realm, setRealm] = useState(null);
+  const startLoading = async () => {
+    const connection = await Realm.open({
+      path: "diaryAppDB",
+      schema: [FeelingSchema],
+    });
+    setRealm(connection);
+  };
+  const onFinish = () => setReady(true);
+  if (!ready) {
+    return (
+      <AppLoading
+        onError={console.error}
+        startAsync={startLoading}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
-    <NavigationContainer>
-      <Navigator />
-    </NavigationContainer>
+    <Context.Provider value={realm}>
+      <NavigationContainer>
+        <Navigator />
+      </NavigationContainer>
+    </Context.Provider>
   );
 }
